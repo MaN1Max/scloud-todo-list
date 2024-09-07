87f96ab2726d2122e39696a31070e53cc5e8cd86
MaN1Max <118527003+MaN1Max@users.noreply.github.com>
2024-09-07 21:40:53 +0300

    Desktop page in progress
    Decomposition into components start...

--- src/App.vue
+++ src/App.vue
@@ -95,5 +95,43 @@
       </button>
     </div>
   </div>
-  
+  <div class="d-flex flex-column content-container">
+    <h2 class="header-text">Доска задач</h2>
+    <div class="d-flex flex-row task-desk-block">
+      <div class="d-flex flex-column task-desk-item-block">
+        <div class="d-flex task-desk-item-label">Открыто</div>
+        <div class="d-flex flex-column task-desk-item-list-block">
+          <div class="task-desk-item-in-list" draggable="true">
+            Проснуться, улыбнуться, сделать отжимания, слетать на Марс и прочитать книгу
+          </div>
+          <div class="task-desk-item-in-list" draggable="true">
+            Проснуться, улыбнуться, сделать отжимания, слетать на Марс и прочитать книгу
+          </div>
+          <div class="task-desk-item-in-list" draggable="true">
+            Проснуться, улыбнуться, сделать отжимания, слетать на Марс и прочитать книгу
+          </div>
+        </div>
+      </div>
+      <div class="d-flex flex-column task-desk-item-block">
+        <div class="d-flex task-desk-item-label">В работе</div>
+        <div class="d-flex flex-column task-desk-item-list-block">
+          <div class="task-desk-item-in-list" draggable="true">
+            Проснуться, улыбнуться, сделать отжимания, слетать на Марс и прочитать книгу
+          </div>
+          <div class="task-desk-item-in-list" draggable="true">
+            Проснуться, улыбнуться, сделать отжимания, слетать на Марс и прочитать книгу
+          </div>
+        </div>
+      </div>
+      <div class="d-flex flex-column task-desk-item-block">
+        <div class="d-flex task-desk-item-label">Закрыто</div>
+        <div class="d-flex flex-column task-desk-item-list-block">
+          <div class="task-desk-item-in-list" draggable="true">
+            Проснуться, улыбнуться, сделать отжимания, слетать на Марс и прочитать книгу
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-block"></div>
 </template>
